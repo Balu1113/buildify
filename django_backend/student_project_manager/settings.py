b84f324f062b7702c403bdffd3c13f0d01fb629e
Balu1113 <talatibalaji@gmail.com>
2026-09-12 18:33:10 +0530

    Configure PostgreSQL for Render

--- django_backend/student_project_manager/settings.py
+++ django_backend/student_project_manager/settings.py
@@ -63,11 +63,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "student_project_manager.wsgi.application"
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []
