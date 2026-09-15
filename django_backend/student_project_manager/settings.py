e2001a567d23acc7111a78ea99a02a45d10df684
Balaji Talati <balaji.talati@pranathibs.local>
2026-09-15 20:21:18 +0530

    cm

--- django_backend/student_project_manager/settings.py
+++ django_backend/student_project_manager/settings.py
@@ -87,6 +87,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+X_FRAME_OPTIONS = "ALLOWALL"
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_ALL_ORIGINS = True
